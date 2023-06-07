2efcd3b799f6e0c11472b31eec64ecdaf51d4532
Aayush <gurung087@gmail.com>
2023-06-07 21:08:48 +0545

    Added Contact Email form
    Added the email send functionality to my portfolio and download cv button

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,3 @@
-console.log("working");
-
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     var scrollPosition = window.scrollY;
@@ -10,4 +8,58 @@ window.addEventListener('scroll', function() {
       navbar.classList.remove('scrolled');
     }
   });
+
+
+gsap.from('.animate-introduction', {
+  opacity: 0, 
+  y: -50, 
+  duration: 1, 
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-skills, .animate-education', {
+  opacity: 0,
+  y: -10,
   
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.animate-skills',
+    start: 'top center+=25%',
+    toggleActions: 'restart pause pause none',
+  },
+});
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_bzybheg';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      document.querySelector('.message').textContent="Sent Successfully";
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+gsap.set('.message', { display: 'block' });
+
+    gsap.fromTo(
+      '.message',
+      { y: 0},
+      {
+        y: 2,
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: 'power1.inOut'
+      }
+    );
